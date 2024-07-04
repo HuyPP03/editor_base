c7c76bf6445557decf9c0e1dc93ab9aa8fdb9bca
Huy PP <phuhuyqhqb@gmail.com>
2024-07-04 20:18:43 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,455 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState, useEffect, useRef } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import axios from "axios";
+
+import {
+  ClassicEditor,
+  AccessibilityHelp,
+  Alignment,
+  Autoformat,
+  AutoImage,
+  AutoLink,
+  Autosave,
+  BlockQuote,
+  Bold,
+  Code,
+  CodeBlock,
+  Essentials,
+  FindAndReplace,
+  FontBackgroundColor,
+  FontColor,
+  FontFamily,
+  FontSize,
+  FullPage,
+  GeneralHtmlSupport,
+  Heading,
+  Highlight,
+  HorizontalLine,
+  HtmlComment,
+  HtmlEmbed,
+  ImageBlock,
+  ImageCaption,
+  ImageInline,
+  ImageInsert,
+  ImageInsertViaUrl,
+  ImageResize,
+  ImageStyle,
+  ImageTextAlternative,
+  ImageToolbar,
+  ImageUpload,
+  Indent,
+  IndentBlock,
+  Italic,
+  Link,
+  LinkImage,
+  List,
+  ListProperties,
+  Markdown,
+  MediaEmbed,
+  PageBreak,
+  Paragraph,
+  PasteFromMarkdownExperimental,
+  PasteFromOffice,
+  RemoveFormat,
+  SelectAll,
+  ShowBlocks,
+  SimpleUploadAdapter,
+  SourceEditing,
+  SpecialCharacters,
+  SpecialCharactersArrows,
+  SpecialCharactersCurrency,
+  SpecialCharactersEssentials,
+  SpecialCharactersLatin,
+  SpecialCharactersMathematical,
+  SpecialCharactersText,
+  Strikethrough,
+  Style,
+  Subscript,
+  Superscript,
+  Table,
+  TableCellProperties,
+  TableColumnResize,
+  TableProperties,
+  TableToolbar,
+  TextTransformation,
+  TodoList,
+  Underline,
+  Undo,
+} from "ckeditor5";
+
+import "ckeditor5/ckeditor5.css";
+
+import "./ckeditor-5.css";
+class UploadAdapter {
+  loader: any;
+  constructor(loader: any) {
+    this.loader = loader;
+  }
+  upload() {
+    return new Promise((resolve, reject) => {
+      this.loader.file.then((file: any) => {
+        const formData = new FormData();
+        formData.append("file", file);
+        axios
+          .post("http://localhost:8080/api/blog/upload", formData, {
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            if (response.data) {
+              resolve({
+                default: `http://localhost:8080/uploads/${response.data.url}`,
+              });
+            } else {
+              reject(new Error("Upload failed"));
+            }
+          })
+          .catch((error) => {
+            reject(error);
+          });
+      });
+    });
+  }
+}
+
+function CustomUploadAdapterPlugin(editor: any) {
+  editor.plugins.get("FileRepository").createUploadAdapter = (loader: any) => {
+    return new UploadAdapter(loader);
+  };
+}
+export default function App() {
+  const editorContainerRef = useRef<HTMLDivElement>(
+    document.createElement("div")
+  );
+  const editorRef = useRef<HTMLDivElement>(document.createElement("div"));
+
+  const [isLayoutReady, setIsLayoutReady] = useState(false);
+  const [editorData, setEditorData] = useState("");
+
+  useEffect(() => {
+    setIsLayoutReady(true);
+
+    return () => setIsLayoutReady(false);
+  }, []);
+
+  console.log(editorData);
+  const editorConfig: any = {
+    toolbar: {
+      items: [
+        "undo",
+        "redo",
+        "|",
+        "sourceEditing",
+        "showBlocks",
+        "findAndReplace",
+        "selectAll",
+        "|",
+        "heading",
+        "style",
+        "|",
+        "fontSize",
+        "fontFamily",
+        "fontColor",
+        "fontBackgroundColor",
+        "|",
+        "bold",
+        "italic",
+        "underline",
+        "strikethrough",
+        "subscript",
+        "superscript",
+        "code",
+        "removeFormat",
+        "|",
+        "specialCharacters",
+        "horizontalLine",
+        "pageBreak",
+        "link",
+        "insertImage",
+        "insertImageViaUrl",
+        "mediaEmbed",
+        "insertTable",
+        "highlight",
+        "blockQuote",
+        "codeBlock",
+        "htmlEmbed",
+        "|",
+        "alignment",
+        "|",
+        "bulletedList",
+        "numberedList",
+        "todoList",
+        "indent",
+        "outdent",
+        "|",
+        "accessibilityHelp",
+      ],
+      shouldNotGroupWhenFull: false,
+    },
+    plugins: [
+      AccessibilityHelp,
+      Alignment,
+      Autoformat,
+      AutoImage,
+      AutoLink,
+      Autosave,
+      BlockQuote,
+      Bold,
+      Code,
+      CodeBlock,
+      Essentials,
+      FindAndReplace,
+      FontBackgroundColor,
+      FontColor,
+      FontFamily,
+      FontSize,
+      FullPage,
+      GeneralHtmlSupport,
+      Heading,
+      Highlight,
+      HorizontalLine,
+      HtmlComment,
+      HtmlEmbed,
+      ImageBlock,
+      ImageCaption,
+      ImageInline,
+      ImageInsert,
+      ImageInsertViaUrl,
+      ImageResize,
+      ImageStyle,
+      ImageTextAlternative,
+      ImageToolbar,
+      ImageUpload,
+      Indent,
+      IndentBlock,
+      Italic,
+      Link,
+      LinkImage,
+      List,
+      ListProperties,
+      Markdown,
+      MediaEmbed,
+      PageBreak,
+      Paragraph,
+      PasteFromMarkdownExperimental,
+      PasteFromOffice,
+      RemoveFormat,
+      SelectAll,
+      ShowBlocks,
+      SimpleUploadAdapter,
+      SourceEditing,
+      SpecialCharacters,
+      SpecialCharactersArrows,
+      SpecialCharactersCurrency,
+      SpecialCharactersEssentials,
+      SpecialCharactersLatin,
+      SpecialCharactersMathematical,
+      SpecialCharactersText,
+      Strikethrough,
+      Style,
+      Subscript,
+      Superscript,
+      Table,
+      TableCellProperties,
+      TableColumnResize,
+      TableProperties,
+      TableToolbar,
+      TextTransformation,
+      TodoList,
+      Underline,
+      Undo,
+    ],
+    mediaEmbed: {
+      previewsInData: true, // Điều này sẽ cho phép các URL oEmbed được hiển thị
+    },
+    fontFamily: {
+      supportAllValues: true,
+    },
+    fontSize: {
+      options: [10, 12, 14, "default", 18, 20, 22],
+      supportAllValues: true,
+    },
+    heading: {
+      options: [
+        {
+          model: "paragraph",
+          title: "Paragraph",
+          class: "ck-heading_paragraph",
+        },
+        {
+          model: "heading1",
+          view: "h1",
+          title: "Heading 1",
+          class: "ck-heading_heading1",
+        },
+        {
+          model: "heading2",
+          view: "h2",
+          title: "Heading 2",
+          class: "ck-heading_heading2",
+        },
+        {
+          model: "heading3",
+          view: "h3",
+          title: "Heading 3",
+          class: "ck-heading_heading3",
+        },
+        {
+          model: "heading4",
+          view: "h4",
+          title: "Heading 4",
+          class: "ck-heading_heading4",
+        },
+        {
+          model: "heading5",
+          view: "h5",
+          title: "Heading 5",
+          class: "ck-heading_heading5",
+        },
+        {
+          model: "heading6",
+          view: "h6",
+          title: "Heading 6",
+          class: "ck-heading_heading6",
+        },
+      ],
+    },
+    htmlSupport: {
+      allow: [
+        {
+          name: /^.*$/,
+          styles: true,
+          attributes: true,
+          classes: true,
+        },
+      ],
+    },
+    image: {
+      toolbar: [
+        "toggleImageCaption",
+        "imageTextAlternative",
+        "|",
+        "imageStyle:inline",
+        "imageStyle:wrapText",
+        "imageStyle:breakText",
+        "|",
+        "resizeImage",
+      ],
+    },
+    link: {
+      addTargetToExternalLinks: true,
+      defaultProtocol: "https://",
+      decorators: {
+        toggleDownloadable: {
+          mode: "manual",
+          label: "Downloadable",
+          attributes: {
+            download: "file",
+          },
+        },
+      },
+    },
+    list: {
+      properties: {
+        styles: true,
+        startIndex: true,
+        reversed: true,
+      },
+    },
+    menuBar: {
+      isVisible: true,
+    },
+    placeholder: "Type or paste your content here!",
+    style: {
+      definitions: [
+        {
+          name: "Article category",
+          element: "h3",
+          classes: ["category"],
+        },
+        {
+          name: "Title",
+          element: "h2",
+          classes: ["document-title"],
+        },
+        {
+          name: "Subtitle",
+          element: "h3",
+          classes: ["document-subtitle"],
+        },
+        {
+          name: "Info box",
+          element: "p",
+          classes: ["info-box"],
+        },
+        {
+          name: "Side quote",
+          element: "blockquote",
+          classes: ["side-quote"],
+        },
+        {
+          name: "Marker",
+          element: "span",
+          classes: ["marker"],
+        },
+        {
+          name: "Spoiler",
+          element: "span",
+          classes: ["spoiler"],
+        },
+        {
+          name: "Code (dark)",
+          element: "pre",
+          classes: ["fancy-code", "fancy-code-dark"],
+        },
+        {
+          name: "Code (bright)",
+          element: "pre",
+          classes: ["fancy-code", "fancy-code-bright"],
+        },
+      ],
+    },
+    table: {
+      contentToolbar: [
+        "tableColumn",
+        "tableRow",
+        "mergeTableCells",
+        "tableProperties",
+        "tableCellProperties",
+      ],
+    },
+    extraPlugins: [CustomUploadAdapterPlugin],
+  };
+
+  return (
+    <div>
+      <div className="main-container">
+        <div
+          className="editor-container editor-container_classic-editor editor-container_include-style"
+          ref={editorContainerRef}
+        >
+          <div className="editor-container__editor">
+            <div ref={editorRef}>
+              {isLayoutReady && (
+                <CKEditor
+                  editor={ClassicEditor}
+                  config={editorConfig}
+                  onChange={(event, editor) => {
+                    const data = editor.getData();
+                    setEditorData(data);
+                  }}
+                />
+              )}
+            </div>
+            <div
+              dangerouslySetInnerHTML={{ __html: editorData }}
+              className="ck-content"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
